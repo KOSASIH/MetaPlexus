6a9477cb50022313fbd3b17ca9321646c443cb3d
KOSASIH <kosasihg88@gmail.com>
2024-09-01 10:19:01 +0700

    Create atomicSwapActions.js

---
+++ meta-plexus-cross-chain-atomic-swaps/frontend/src/actions/atomicSwapActions.js
@@ -0,0 +1,7 @@
+export const initAtomicSwap = () => async (dispatch) => {
+    // initialize atomic swap contract instance
+};
+
+export const swapTokens = (tokenA, tokenB, amountA, amountB) => async (dispatch) => {
+    // call atomic swap contract function
+};
