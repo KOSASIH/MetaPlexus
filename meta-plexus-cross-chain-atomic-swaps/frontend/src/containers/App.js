23ce1156944fba2f9e0f6f0684eda410d802505a
KOSASIH <kosasihg88@gmail.com>
2024-09-01 10:18:25 +0700

    Create App.js

---
+++ meta-plexus-cross-chain-atomic-swaps/frontend/src/containers/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AtomicSwap from '../components/AtomicSwap';
+
+const App = () => {
+    return (
+        <div>
+            <h1>MetaPlexus Cross-Chain Atomic Swaps</h1>
+            <AtomicSwap />
+        </div>
+    );
+};
+
+export default App;
