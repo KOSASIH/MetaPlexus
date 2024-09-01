b042a79aff9560940eefb58269b97b06a06c6fe4
KOSASIH <kosasihg88@gmail.com>
2024-09-01 09:48:51 +0700

    Update Token.sol

--- blockchain_integration/pi_network/contracts/Token.sol
+++ blockchain_integration/pi_network/contracts/Token.sol
@@ -1,50 +1,69 @@
 pragma solidity ^0.8.0;
 
-import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-solidity/contracts/token/ERC20/SafeERC20.sol";
 
-contract Token is ERC20 {
-    // Name of the token
-    string public name = "MetaPlexus Token";
-
-    // Symbol of the token
-    string public symbol = "MPT";
-
-    // Decimals of the token
-    uint8 public decimals = 18;
-
-    // Total supply of the token
-    uint256 public totalSupply = 100000000 * (10 ** decimals);
-
-    // Mapping of user addresses to their balances
+contract Token is SafeERC20 {
+    // Mapping of token balances
     mapping (address => uint256) public balances;
 
-    // Event emitted when tokens are transferred
-    event Transfer(address indexed from, address indexed to, uint256 value);
+    // Mapping of token allowances
+    mapping (address => mapping (address => uint256)) public allowances;
 
-    // Event emitted when tokens are approved for spending
+    // Token name
+    string public name;
+
+    // Token symbol
+    string public symbol;
+
+    // Token total supply
+    uint256 public totalSupply;
+
+    // Reentrancy protection
+    bool private reentrancyLock;
+
+    // Events
+    event Transfer(address indexed from, address indexed to, uint256 value);
     event Approval(address indexed owner, address indexed spender, uint256 value);
 
-    // Function to transfer tokens
-    function transfer(address to, uint256 value) public {
-        // Check if the user has sufficient balance
-        require(balances[msg.sender] >= value, "Insufficient balance");
-
-        // Update the user's balance
-        balances[msg.sender] = balances[msg.sender].sub(value);
-
-        // Update the recipient's balance
-        balances[to] = balances[to].add(value);
-
-        // Emit the Transfer event
-        emit Transfer(msg.sender, to, value);
+    // Constructor
+    constructor(string memory _name, string memory _symbol, uint256 _totalSupply) public {
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply;
+        balances[msg.sender] = totalSupply;
     }
 
-    // Function to approve tokens for spending
-    function approve(address spender, uint256 value) public {
-        // Update the allowance
-        allowances[msg.sender][spender] = value;
+    // Transfer function with reentrancy protection
+    function transfer(address to, uint256 value) public {
+        require(!reentrancyLock, "Reentrancy lock is enabled");
+        reentrancyLock = true;
+        _transfer(msg.sender, to, value);
+        reentrancyLock = false;
+    }
 
-        // Emit the Approval event
+    // Transfer function with input validation
+    function _transfer(address from, address to, uint256 value) internal {
+        require(from != address(0), "From address is zero");
+        require(to != address(0), "To address is zero");
+        require(value > 0, "Value is zero");
+        require(balances[from] >= value, "Insufficient balance");
+        balances[from] -= value;
+        balances[to] += value;
+        emit Transfer(from, to, value);
+    }
+
+    // Approval function with input validation
+    function approve(address spender, uint256 value) public {
+        require(spender != address(0), "Spender address is zero");
+        require(value > 0, "Value is zero");
+        allowances[msg.sender][spender] = value;
         emit Approval(msg.sender, spender, value);
+    }
+
+    // Allowance function with input validation
+    function allowance(address owner, address spender) public view returns (uint256) {
+        require(owner != address(0), "Owner address is zero");
+        require(spender != address(0), "Spender address is zero");
+        return allowances[owner][spender];
     }
 }
