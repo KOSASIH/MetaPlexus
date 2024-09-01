369e90d227b0b6786fd07e24a12901681c2335b3
KOSASIH <kosasihg88@gmail.com>
2024-09-01 10:05:00 +0700

    Create solana-cli.config.js

---
+++ blockchain_integration/solana-cli/solana-cli.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  network: 'Solana',
+  rpcUrl: 'https://api.mainnet-beta.solana.com/',
+  chainId: 101,
+  accounts: [
+    {
+      privateKey: 'YOUR_PRIVATE_KEY_HERE',
+      address: 'YOUR_ADDRESS_HERE',
+    },
+  ],
+};
