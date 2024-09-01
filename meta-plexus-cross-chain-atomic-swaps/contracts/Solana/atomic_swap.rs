0e2fafcaa19ea9aeda8bcec7fcc8d09c39db064b
KOSASIH <kosasihg88@gmail.com>
2024-09-01 10:15:35 +0700

    Create atomic_swap.rs

---
+++ meta-plexus-cross-chain-atomic-swaps/contracts/Solana/atomic_swap.rs
@@ -0,0 +1,19 @@
+use solana_program::{
+    account_info::{next_account_info, AccountInfo},
+    entrypoint,
+    entrypoint::ProgramResult,
+    msg,
+    program_error::ProgramError,
+    pubkey::Pubkey,
+};
+
+entrypoint!(process_instruction);
+
+fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    // implement atomic swap logic here
+    Ok(())
+}
