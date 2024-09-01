5a6a29756edfd50b64eefdc26badd4fe8a842956
KOSASIH <kosasihg88@gmail.com>
2024-09-01 10:03:04 +0700

    Create bnb-cli.config.js

---
+++ blockchain_integration/bnb-cli/bnb-cli.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  network: 'BSC',
+  rpcUrl: 'https://bsc-dataseed.binance.org/',
+  chainId: 56,
+  gasPrice: 20e9,
+  gasLimit: 8000000,
+  accounts: [
+    {
+      mnemonic: 'YOUR_MNEMONIC_HERE',
+      address: 'YOUR_ADDRESS_HERE',
+    },
+  ],
+};
