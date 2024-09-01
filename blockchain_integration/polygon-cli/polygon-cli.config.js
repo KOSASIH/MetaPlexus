3c0fbd2ec531dbe4b44d4204a520070234533b42
KOSASIH <kosasihg88@gmail.com>
2024-09-01 10:03:59 +0700

    Create polygon-cli.config.js

---
+++ blockchain_integration/polygon-cli/polygon-cli.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  network: 'Polygon',
+  rpcUrl: 'https://matic-mainnet.chainstacklabs.com/',
+  chainId: 137,
+  gasPrice: 20e9,
+  gasLimit: 8000000,
+  accounts: [
+    {
+      mnemonic: 'YOUR_MNEMONIC_HERE',
+      address: 'YOUR_ADDRESS_HERE',
+    },
+  ],
+};
